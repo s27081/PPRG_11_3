408f63f726712302d0522cff5b3cca21969fff22
s27081 <115358253+s27081@users.noreply.github.com>
2022-11-13 16:13:04 +0100

    Update zad3.3.cpp

--- zad3.3.cpp
+++ zad3.3.cpp
@@ -21,24 +21,32 @@ int main() {
     cin >> B;
     cout << endl << "Podaj parametr C: ";
     cin >> C;
-   
-    string func = to_string(A) +"x^2+" + to_string(B) + 'x' + '+' + to_string(C);
+
+    string func = to_string(A) + "x^2+" + to_string(B) + 'x' + '+' + to_string(C);
 
     cout << "Postac ogolna funkcji kwadratowej z parametrami A,B i C: " << func << endl;
 
     int delta = (B * B) - (4 * A * C);
 
     if (delta > 0) {
-        float x1 = (B * (-1) - sqrt(delta)) / (2 * A);
-        float x2 = (B * (-1) + sqrt(delta)) / (2 * A);
-
+        double x1 = (B * (-1) - sqrt(delta)) / (2 * A);
+        double x2 = (B * (-1) + sqrt(delta)) / (2 * A);
+        cout << "Delta > 0" << endl;
+        cout << "x1: " << x1 << endl;
+        cout << "x2:" << x2 << endl;
 
     }
     else if (delta == 0) {
         float x = B * (-1) / (2 * A);
+
+        cout << "Delta = 0, x:" << x << endl;
     }
 
-   
+    else if (delta < 0) {
+        cout << "Delta < 0, rownanie nie spelnia zalozen" << endl; 
+    }
+
+
 
 
     return 0;
