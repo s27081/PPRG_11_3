2ed770f2a1bcf2b61d07ffb8957aac29371ad3d8
s27081 <115358253+s27081@users.noreply.github.com>
2022-11-16 20:07:42 +0100

    Add files via upload

---
+++ zad3.4.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+
+    int rows;
+    int content = 1;
+
+    cout << "Podaj rozmiar trojkata Pascala: ";
+       cin >> rows;
+
+       for (int i=0; i <= rows; i++) {
+           content = 1;
+           for (int k = rows - i; k > 0; k--) {
+               cout << " ";
+           }
+           for (int j = 1; j <= i; j++) {
+               cout << content << " ";
+               content = content*(i - j)/j;
+               }
+           cout << endl;
+           }
+
+     return 0;
+    }
